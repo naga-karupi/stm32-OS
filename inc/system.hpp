c78e8624acfeaa04aca3817cf306401e6df39107
naga <toritonsuibu0527@gmail.com>
2023-09-22 23:52:43 +0900

    system.hppを追加

---
+++ inc/system.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file system.hpp
+ * @brief メインシステムを提供します。
+ * @version 0.0
+ * @date 2023-09-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "generator.hpp"
+
+#pragma once
+
+
