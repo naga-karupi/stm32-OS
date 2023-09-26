5475e8fe50e514a848ed13bf69cd5bd3bc1c1858
naga <toritonsuibu0527@gmail.com>
2023-09-26 21:32:56 +0900

    コメント追加と設計変更に伴う変更

--- inc/atomic.hpp
+++ inc/atomic.hpp
@@ -11,30 +11,82 @@
 
 #include <concepts>
 #include <utility>
+#include <memory>
 
 template <std::copyable Copyable>
 class Atomic
 {
 public:
-    Atomic(Copyable);
-    Atomic(Atomic&&);
+    /**
+     * @brief Construct a new Atomic object
+     * 
+     */
+    Atomic(const Copyable);
+
+    /**
+     * @brief Construct a new Atomic object
+     *        ムーブ代入のみ許可する
+     * 
+     */
+    Atomic(const Atomic&&);
 
     ~Atomic();
 
+    /**
+     * @brief 本来はロックフリーであることを示すが、
+     *        ミューテックスによるデッドロックが
+     *        起こりえないので常にtrueを返す
+     * 
+     * @return true always
+     * @return false none
+     */
     bool isLockFree();
     
-    void Store(Copyable);
+    /**
+     * @brief 値を書き込む
+     * 
+     * @param cp 書き込む値
+     */
+    void Store(const Copyable cp);
 
+    /**
+     * @brief 値を読み出す
+     * 
+     * @return Copyable 読み出された値
+     */
     Copyable Load();
 
+    /**
+     * @brief キャスト可能な型にキャストする
+     * 
+     * @tparam T キャスト可能な型
+     * @return T キャストされた保持されている値
+     */
     template<typename T>
     operator T();
 
-    Copyable Exchange(Copyable);
+    /**
+     * @brief 値を交換する
+     * 
+     * @param desired 入れ替える値
+     * @return Copyable もと入っている値
+     */
+    Copyable Exchange(const Copyable desired);
 
-    Copyable CompareExchange(Copyable);
+    /**
+     * @brief 保持している値が期待した値だった場合、保持している値を更新し、trueを返す
+     *        そうでなければ保持している値を更新せずにfalseを返す
+     * 
+     * @param expected 期待される値
+     * @param desired 代入する値
+     * @return true 値の更新に成功
+     * @return false 値の更新に失敗
+     */
+    bool CompareExchange(const Copyable& expected, const Copyable desired);
 
 private:
+    Copyable m_entity;
+
     Atomic(Atomic&) = delete;
     void operator = (Atomic&) = delete;
     void operator = (Atomic&&) = delete;
